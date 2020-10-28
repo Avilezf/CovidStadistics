8a1c2ee15d3adad541cb4a12759092626c00018d
luisl <luisl@PC4>
2020-10-27 22:27:59 -0500

    2st commit

---
+++ CovidStadistics/src/main/java/Controller/ServletControlador.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Ing. Luis Llanos / BNT SAS
+ */
+@WebServlet("/ServletControlador")
+public class ServletControlador extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String accion = request.getParameter("accion");
+        String path = "/pages/dashboard.jsp";
+        request.getRequestDispatcher(path).forward(request, response);
+        response.sendRedirect("/pages/dashboard.jsp");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String accion = request.getParameter("accion");
+
+    }
+
+}
